7ddc5f319144ad22c454ed5b6b10ac74def40c05
Adha Bakhtiar <adhabakhtiar@gmail.com>
2020-09-03 16:19:22 +0700

    modified:   package.json 	modified:   pages/_app.js 	modified:   pages/index.js 	modified:   styles/globals.css 	components/ 	jsconfig.json 	module/ 	pages/auth/ 	providers/ 	public/bg.jpg 	styles/Login.module.scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
+import UserProvider from "../providers/users-provider";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <UserProvider>
+    <Component {...pageProps} />
+  </UserProvider>
+  )
 }
 
 export default MyApp
